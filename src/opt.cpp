5a66f57c22a792f4994c9250b4a10dafdbec711f
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2017-09-05 13:43:53 +0000

    Added option to disable safety checks for a 5% increase in speed. Added the reprojection error to the my own code

--- src/opt.cpp
+++ src/opt.cpp
@@ -4,7 +4,7 @@
 #include "ceres/ceres.h"
 #include "ceres/rotation.h"
 #include "bal_problem.h"
-#include "snavely_reprojection_error.h"
+#include "ceres_analytical/snavely_reprojection_error.h"
 
 #include <chrono>
 
@@ -22,26 +22,32 @@ int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   if (argc != 2) { std::cerr << "usage: simple_bundle_adjuster <bal_problem>\n"; return 1; }
 
+  //Solver options
+  ceres::Solver::Options solver_options;
+  solver_options.linear_solver_type = ceres::DENSE_SCHUR;
+  solver_options.minimizer_progress_to_stdout = true;
+
+  //problem options
+  Problem::Options problem_options;
+  // problem_options.disable_all_safety_checks=true; //Gain 5% in speed
+
 
   //READ file
   BALProblem bal_problem(argv[1],false); //Second argument is for using quaternions or not
 
 
   //create problem
-  Problem problem;
+  Problem problem(problem_options);
   bal_problem.Normalize(); //TODO check if necesary
   BuildProblem(&bal_problem, &problem);
 
 
-  //options
-  ceres::Solver::Options options;
-  options.linear_solver_type = ceres::DENSE_SCHUR;
-  options.minimizer_progress_to_stdout = true;
+
 
 
   //solve
   std::chrono::high_resolution_clock::time_point t1_opt = std::chrono::high_resolution_clock::now();
-  ceres::Solve(options, &problem, &summary);
+  ceres::Solve(solver_options, &problem, &summary);
   std::chrono::high_resolution_clock::time_point t2_opt = std::chrono::high_resolution_clock::now();
 
 
@@ -50,6 +56,8 @@ int main(int argc, char** argv) {
   std::cout << "total took " << duration_opt * 1e-6<< " s " << std::endl;
   // std::cout << summary.FullReport() << "\n";
 
+  //report final parameters
+
 
   return 0;
 }
@@ -57,10 +65,11 @@ int main(int argc, char** argv) {
 
 
 void BuildProblem(BALProblem* bal_problem, Problem* problem) {
-  const int point_block_size = bal_problem->point_block_size();
-  const int camera_block_size = bal_problem->camera_block_size();
+  const int point_block_size = bal_problem->point_block_size();    //point is 3 parameters
+  const int camera_block_size = bal_problem->camera_block_size();  //camera is 9 parameters
   double* points = bal_problem->mutable_points();
   double* cameras = bal_problem->mutable_cameras();
+
   // Observations is 2*num_observations long array observations =
   // [u_1, u_2, ... , u_n], where each u_i is two dimensional, the x
   // and y positions of the observation.
