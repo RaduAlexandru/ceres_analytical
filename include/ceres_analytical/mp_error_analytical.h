b8d59e3a4a9fa6d1201d5ab283dae4ccc83bd787
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2017-11-17 14:10:07 +0000

    Cleaned up code and optimized

--- include/ceres_analytical/mp_error_analytical.h
+++ include/ceres_analytical/mp_error_analytical.h
@@ -39,12 +39,12 @@ class ErrorAnalyticalOptK : public SizedCostFunction<2, /* number of residuals *
     //NO DISTORTION
 
     // Compute final projected point position.
-    const double & focal_x = m_intrinsics[0];
-    const double & focal_y = m_intrinsics[1];
+    const double & fx = m_intrinsics[0];
+    const double & fy = m_intrinsics[1];
     const double & c_x = m_intrinsics[2];
     const double & c_y = m_intrinsics[3];
-    const double predicted_x = focal_x * xp + c_x;
-    const double predicted_y = focal_y * yp + c_y;
+    const double predicted_x = fx * xp + c_x;
+    const double predicted_y = fy * yp + c_y;
 
 
     // The error is the difference between the predicted and observed position.
@@ -53,99 +53,67 @@ class ErrorAnalyticalOptK : public SizedCostFunction<2, /* number of residuals *
     //std::cout << " residual is " << residuals[0] << ", " << residuals[1] << '\n';
 
 
-
-    //Blanco's notation
-    double fx=focal_x;
-    double fy=focal_y;
-    double gx=p3d_local[0];
-    double gy=p3d_local[1];
-    double gz=p3d_local[2];
-    double gx2=gx*gx;
-    double gy2=gy*gy;
-    double gz2=gz*gz;
-    Eigen::Matrix3d R;
-    double R_array[9];
-    QuaternionToRotation(qvec,R_array);
-    R=Eigen::Map<Eigen::Matrix<double,3,3,Eigen::RowMajor> >(R_array);
-
-    Eigen::MatrixXd jacobian_projection(2,3);
-    Eigen::MatrixXd jacobian_point3d(2,3);
-
-    jacobian_projection(0,0)=fx/gz;
-    jacobian_projection(0,1)=0.0;
-    jacobian_projection(0,2)=-gx/gz2*fx;
-
-    jacobian_projection(1,0)=0.0;
-    jacobian_projection(1,1)=fy/gz;
-    jacobian_projection(1,2)=-gy/gz2*fy;
+    //precompute stuff (this are the entries of the jacobian of the projection)
+    double z_2=p3d_local[2]*p3d_local[2];
+    double j_proj_00=fx/ p3d_local[2];
+    double j_proj_11=fy/ p3d_local[2];
+    double j_proj_02=-fx*p3d_local[0]/ z_2;
+    double j_proj_12=-fy*p3d_local[1]/ z_2;
 
 
     if ( jacobians != NULL )
     {
 
 
-      const double z_2 = p3d_local[2]*p3d_local[2];
       if ( jacobians[0] != NULL) {
 
-        // //jacobian of the pose (2x7)
-        // MatrixRef j_eigen(jacobians[0], 2, 7);
-        // j_eigen.setZero();
-        // Eigen::Vector3d g={p3d_only_rot[0],p3d_only_rot[1],p3d_only_rot[2]};
-        // Eigen::Matrix3d g_hat;
-        // g_hat << 0.0, -g(2), g(1),
-        //         g(2), 0.0, -g(0),
-        //         -g(1), g(0), 0.0;
-        //
-        // Eigen::MatrixXd right_side(3,7);
-        // right_side.setZero();
-        // right_side.block(0,0,3,3)=-g_hat*2;  //TODO why is it scaled down by a half?
-        // right_side.block(0,3,3,3)=Eigen::Matrix3d::Identity();
-        //
-        // j_eigen=jacobian_projection*right_side;
+        //precompute some more stuff
+        double x_rot_2=p3d_only_rot[0]*2;
+        double y_rot_2=p3d_only_rot[1]*2;
+        double z_rot_2=p3d_only_rot[2]*2;
 
-
-
-        // //Without eigen matrices
-        // block of rotation x
-        jacobians[0][0]= -focal_x *   p3d_local[0]*p3d_only_rot[1]/z_2*2; // -fx*gx*gy/gz2
-        jacobians[0][1]=  focal_x/p3d_local[2]*p3d_only_rot[2]*2 + focal_x*p3d_local[0]/z_2*p3d_only_rot[0]*2;
-        jacobians[0][2]= -focal_x/p3d_local[2]*p3d_only_rot[1]*2;
+        //block of rotation x
+        jacobians[0][0]=j_proj_02*y_rot_2;
+        jacobians[0][1]=j_proj_00*z_rot_2 - j_proj_02*x_rot_2;
+        jacobians[0][2]=-j_proj_00*y_rot_2;
 
         // translation x
-        jacobians[0][3]=  focal_x /   p3d_local[2];
+        jacobians[0][3]= j_proj_00;
         jacobians[0][4]= 0.0;
-        jacobians[0][5]= -focal_x *   p3d_local[0] / z_2;
+        jacobians[0][5]= j_proj_02;
 
-        // block or rotation y
-        jacobians[0][7]= -focal_y/p3d_local[2]*p3d_only_rot[2]*2 - focal_y*p3d_local[1]/z_2*p3d_only_rot[1]*2;
-        jacobians[0][8]= focal_y *   p3d_local[1]*p3d_only_rot[0]/z_2*2;//fy*gx*gy/gz2
-        jacobians[0][9]=  focal_y *   p3d_only_rot[0]/p3d_local[2]*2; // fy*gx/gz
+
+        // block of rotation y
+        jacobians[0][7]= -j_proj_11*z_rot_2 + j_proj_12*y_rot_2;
+        jacobians[0][8]= -j_proj_12*x_rot_2;
+        jacobians[0][9]=  j_proj_11*x_rot_2;
 
         //block of translation y
         jacobians[0][10]= 0.0;
-        jacobians[0][11]= focal_y /   p3d_local[2];
-        jacobians[0][12]=-focal_y *   p3d_local[1] / z_2;
+        jacobians[0][11]= j_proj_11;
+        jacobians[0][12]= j_proj_12;
 
         //last two zeros
         jacobians[0][6] = 0.0;
         jacobians[0][13]= 0.0;
 
 
-
-
       }
 
       if ( jacobians[1] != NULL) {
-         //precompute stuff
-         //jacobian of p3d is of size 2x3 , fill in a row majow order jacobians[0][0...5]
-       jacobian_point3d= jacobian_projection*R;
-       jacobians[1][0] = jacobian_point3d(0,0);
-       jacobians[1][1] = jacobian_point3d(0,1);
-       jacobians[1][2] = jacobian_point3d(0,2);
 
-       jacobians[1][3] = jacobian_point3d(1,0);
-       jacobians[1][4] = jacobian_point3d(1,1);
-       jacobians[1][5] = jacobian_point3d(1,2);
+         //precompute stuff
+         double R_array[9];
+         QuaternionToRotation(qvec,R_array);
+
+         //jacobian of p3d is of size 2x3 , fill in a row majow order jacobians[0][0...5]
+        jacobians[1][0]= j_proj_00*R_array[0] + j_proj_02*R_array[6];
+        jacobians[1][1]= j_proj_00*R_array[1] + j_proj_02*R_array[7];
+        jacobians[1][2]= j_proj_00*R_array[2] + j_proj_02*R_array[8];
+
+        jacobians[1][3]= j_proj_11*R_array[3] + j_proj_12*R_array[6];
+        jacobians[1][4]= j_proj_11*R_array[4] + j_proj_12*R_array[7];
+        jacobians[1][5]= j_proj_11*R_array[5] + j_proj_12*R_array[8];
 
 
 
